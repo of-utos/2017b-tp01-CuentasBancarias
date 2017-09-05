d6d394555d8cc5d1dd8ec30410b852286686a688
Oc Frutos <o_frutos@yahoo.com>
2017-09-05 20:37:46 -0300

    ROJO: Se agrega el caso para n = 2

--- test/test/TestCuenta.java
+++ test/test/TestCuenta.java
@@ -15,9 +15,13 @@ public class TestCuenta {
 
 		cuentaOrigen.transferirMontoHacia(1000, cuentaDestino);
 
-		double saldoActual = cuentaDestino.monto;
-
-		assertEquals(1000, saldoActual, 0);
+		double saldoActualDestino = cuentaDestino.monto;
+		double saldoActualOrigen = cuentaDestino.monto;
+		
+		// COMPRUEBO QUE SE HAYA ACREDITADO EL SALDO.
+		assertEquals(1000, saldoActualDestino, 0);
+		
+		// COMPRUEBO QUE LA TRANSFERENCIA SEA COHERENTE ENTRE AMBAS PARTES.
+		assertEquals(saldoActualOrigen + 1000, saldoActualDestino - 1000, 0);
 	}
-
 }
