14b25c24966f594da36ba7174ce5a653340bcd70
Oc Frutos <o_frutos@yahoo.com>
2017-09-05 21:33:59 -0300

    AZUL: Se mejora nombre de variables

--- src/cuenta/Cuenta.java
+++ src/cuenta/Cuenta.java
@@ -1,23 +1,53 @@
 package cuenta;
 
+/**
+ * Clase que administra el monto de una cuenta. <br>
+ */
 public class Cuenta {
-
+	/**
+	 * Monto de la cuenta. <br>
+	 */
 	private double monto;
 
-	public Cuenta(double monto) {
+	/**
+	 * Crea una cuenta con su monto disponible. <br>
+	 * 
+	 * @param monto
+	 *            Monto disponible. <br>
+	 */
+	public Cuenta(final double monto) {
 		this.monto = monto;
 	}
 
-	public void transferirMontoHacia(double monto, Cuenta cuentaDestino) {
+	/**
+	 * Transfiere un monto de una cuenta a otra. <br>
+	 * 
+	 * @param monto
+	 *            Monto a transferir. <br>
+	 * @param cuentaDestino
+	 *            Cuenta a la que transfiere. <br>
+	 */
+	public void transferirMontoHacia(final double monto, final Cuenta cuentaDestino) {
 		cuentaDestino.asignarMonto(monto);
 		this.monto -= monto;
 	}
 
+	/**
+	 * Devuelve el monto disponible de la cuenta. <br>
+	 * 
+	 * @return Monto disponible en la cuenta. <br>
+	 */
 	public double getMonto() {
 		return monto;
 	}
 
-	private void asignarMonto(double monto) {
+	/**
+	 * Asigna una nueva cantidad al monto. <br>
+	 * 
+	 * @param monto
+	 *            Monto a asignar. <br>
+	 */
+	private void asignarMonto(final double monto) {
 		this.monto += monto;
 	}
 }
